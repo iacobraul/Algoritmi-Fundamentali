3bf2370bf8a4d317f10bc3132dcb67655237a342
Raul Iacob <137762504+iacobraul@users.noreply.github.com>
2026-03-12 10:45:29 +0200

    .

---
+++ P267_2/P267_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P267_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TextReader load = new StreamReader(@"..\..\unice.in");
+            TextWriter save = new StreamWriter(@"..\..\unice.out");
+            int n = int.Parse(load.ReadLine());
+            int[] v = new int[100];
+
+            string buffer;
+            while ((buffer = load.ReadLine()) != null)
+            {
+                string[] local = buffer.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string str in local)
+                {
+                    int t = int.Parse(str);
+                    v[t]++;
+                }
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (v[i] == 1)
+                {
+                    save.Write(i + " ");
+                }
+            }
+            save.Close();
+
+        }
+    }
+}
