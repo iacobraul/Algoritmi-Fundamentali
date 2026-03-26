e0f5bcbacb0495f6cd5618fc545ccf793a1e4cc8
Raul Iacob <137762504+iacobraul@users.noreply.github.com>
2026-03-26 11:39:24 +0200

    .

---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{   
+    internal class Program
+    {
+        public class Spectacol
+        {
+            public int ti;
+            public int tf;
+            public Spectacol(int ti, int tf)
+            {
+                this.ti = ti;
+                this.tf = tf;
+            }
+            public Spectacol(string data)
+            {
+                this.ti = int.Parse(data.Split(' ')[0]);
+                this.tf = int.Parse(data.Split(' ')[1]);
+            }
+
+            public string View()
+            {
+                return ti + " " + tf;
+            }
+        }
+        public class T
+        {
+            List<Spectacol> list;
+            public T()
+            {
+                list = new List<Spectacol>();
+            }
+            public void Load(string fileName)
+            {
+                list = new List<Spectacol>();
+                TextReader load = new StreamReader(fileName);
+                string buffer;
+                while ((buffer = load.ReadLine()) != null)
+                {
+                    list.Add(new Spectacol(buffer));
+                }
+                load.Close();
+            }
+            public List<string> View()
+            {
+                List<string> tor = new List<string>();
+                foreach (Spectacol s in list)
+                {
+                    tor.Add(s.View());
+                }
+                return tor;
+            }
+            public void Sort()
+            {
+                for (int i = 0; i < list.Count - 1; i++)
+                {
+                    for (int j = i + 1; j < list.Count; j++)
+                    {
+                        if (list[i].tf > list[j].tf)    (list[i], list[j]) = (list[j], list[i]);
+                    }
+                }
+            }
+            public void Selectie()
+            {
+                Sort();
+                Console.WriteLine(list[0].View());
+                int t = list[0].tf;
+                for (int i = 1; i < list.Count; i++)
+                {
+                    if (list[i].ti >= t)
+                    {
+                        Console.WriteLine(list[i].View());
+                        t = list[i].tf;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            T demo = new T();
+            demo.Load(@"..\..\data.in");
+            
+            demo.Selectie();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab5_ex2/Lab5_ex2/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5_ex2
+{
+    
+    public partial class Form1 : Form
+    {   
+        Graphics grp;
+        Bitmap bmp;
+        T demo;
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            grp = Graphics.FromImage(bmp);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            demo = new T(1000, pictureBox1.Width, pictureBox1.Height);
+            demo.Draw(grp);
+            demo.Greddy(grp);
+            pictureBox1.Image = bmp;
+        }
+    }
+
+    public class MyPointF
+    {
+        public float X, Y;
+        public bool selected;
+        public MyPointF(float x, float y, bool selected)
+        {
+            X = x;
+            Y = y;
+            this.selected = selected;
+        }
+
+        public void Draw(Graphics handler)
+        {
+            handler.DrawEllipse(Pens.Black, X - 2, Y - 2, 4, 4);
+        }
+        public float Distanta(MyPointF d)
+        {
+            return (float)Math.Sqrt((this.X - d.X) * (this.X - d.X) + (this.Y - d.Y) * (this.Y - d.Y));
+        }
+    }
+
+    public class T 
+    {
+        public static Random random = new Random();
+        public MyPointF[] points;
+        public T(int n, int resx, int resy) 
+        {
+            points = new MyPointF[n];
+            for(int i = 0; i < n; i++)
+            {
+                points[i] = new MyPointF(random.Next(resx), random.Next(resy), false);
+            }
+        }
+        public void Draw(Graphics handler)
+        {
+            for(int i = 0; i < points.Length; i++)
+            {
+                points[i].Draw(handler);
+            }
+        }
+        public void Greddy(Graphics handler)
+        {
+            int indexstart = 0;
+
+            for (int j = 0; j < points.Length - 1; j++)
+            {
+                points[indexstart].selected = true;
+                float max = 10000;
+                int index = 0;
+                for (int i = 1; i < points.Length; i++)
+                {
+                    if (points[i].selected == false)
+                    {
+                        if (points[indexstart].Distanta(points[i]) < max)
+                        {
+                            max = points[indexstart].Distanta(points[i]);
+                            index = i;
+                        }
+                    }
+                }
+                handler.DrawLine(Pens.Red, points[indexstart].X, points[indexstart].Y, points[index].X, points[index].Y);
+                points[index].selected = true;
+                indexstart = index;
+            }
+        }
+    }
+   
+}
