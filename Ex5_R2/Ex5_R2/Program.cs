2579b5258a40054cbe0f6f6258c4c1bf3e16b7cd
Raul Iacob <137762504+iacobraul@users.noreply.github.com>
2026-06-14 14:29:45 +0300

    .

---
+++ Ex5_R2/Ex5_R2/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ex5_R2
+{
+    public class MyQueue
+    {
+        public int[] data;
+        public int front, back;
+        public int count;
+        public MyQueue()
+        {
+            data = new int[16];
+            front = 0; back = 0;
+            count = 0;
+        }
+        public void Enqueue(int value)
+        {
+            if (count == data.Length) Resize();
+            for (int i = 0; i < data.Length; i++) 
+            {
+                if (data[i] == value) return;
+            }
+            data[front] = value;
+            front++;
+            count++;
+        }
+        public int Dequeue()
+        {
+            if (count == 0) throw new InvalidOperationException("Queue is empty");
+
+            int tmp = data[back];
+            back++;
+            count--;
+            return tmp;
+        }
+        private void Resize()
+        {
+            int[] newData = new int[data.Length * 2];
+            for (int i = 0; i < data.Length; i++)
+            {
+                newData[i] = data[i];
+            }
+            data = newData;
+        }
+        public int Count => count;
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = back; i < front; i++)
+            {
+                sb.Append(data[i] + " ");
+            }
+            return sb.ToString();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> A = new Stack<int>();
+            Stack<int> C = new Stack<int>();
+            MyQueue B = new MyQueue();
+            TextReader load = new StreamReader(@"../../data.in");
+            string buffer;
+            while ((buffer = load.ReadLine()) != null)
+            {
+                string[] data = buffer.Split(',');
+                foreach (string s in data)
+                {
+                    string operation;
+                    int value = 0;
+                    if (s.Length > 2)
+                    {
+                        string[] parts = s.Split(' ');
+                        operation = parts[0];
+                        value = int.Parse(parts[1]);
+                    }
+                    else
+                    {
+                        operation = s;
+                    }
+                    if (operation == "M1")
+                    {
+                        A.Push(value);
+                    }
+                    else if (operation == "M2")
+                    {
+                        value = A.Pop();
+                        B.Enqueue(value);
+                    }
+                    else if (operation == "M3")
+                    {
+                        value = B.Dequeue();
+                        C.Push(value);
+                    }
+                    else if (operation == "M4")
+                    {
+                        Console.WriteLine(C.Pop());
+                    }
+                }//foreach
+            }//while
+            Console.Write("A: ");
+            foreach (int a in A)
+            {
+                Console.Write(a + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine($"B: {B}");
+
+            Console.Write("C: ");
+            foreach (int c in C)
+            {
+                Console.Write(c + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Ex7_R2/Ex7_R2/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Ex7_R2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            PointF center = new PointF(this.ClientSize.Width / 2, this.ClientSize.Height / 2);
+            int radius = 120;
+            Draw(g, center, radius);
+
+        }
+        public void Draw(Graphics g, PointF center, int radius) 
+        {
+            PointF[] points = new PointF[6];
+            points[0] = new PointF(center.X, center.Y - radius);
+            points[3] = new PointF(center.X, center.Y + radius);
+            points[1] = new PointF(center.X + (int)Math.Sqrt(Math.Pow(radius,2)-Math.Pow(radius/2,2)), center.Y - radius/2);
+            points[2] = new PointF(center.X + (int)Math.Sqrt(Math.Pow(radius, 2) - Math.Pow(radius / 2, 2)), center.Y + radius / 2);
+            points[4] = new PointF(center.X - (int)Math.Sqrt(Math.Pow(radius, 2) - Math.Pow(radius / 2, 2)), center.Y + radius / 2);
+            points[5] = new PointF(center.X - (int)Math.Sqrt(Math.Pow(radius, 2) - Math.Pow(radius / 2, 2)), center.Y - radius / 2);
+
+            if(radius < 10)
+            {
+                Pen p = new Pen(Color.Black, 3);
+                g.DrawPolygon(p, points);
+            }
+            else
+            {
+                foreach (PointF point in points)
+                {
+                    Draw(g, point, radius / 3);
+                }
+            }
+        }
+        public float Dist(PointF A, PointF B)
+        {
+            return (float)Math.Sqrt( Math.Pow(A.X+B.X,2) - Math.Pow(A.Y+B.Y,2) );
+        }
+    }
+}
